0214c1c892f1b3cfec51fbc440823b72c3841f84
Seunghyun-Jeong <dkskakdgka@gmail.com>
2022-07-07 18:41:05 +0900

    추가과제 커밋

--- tutorial-step-1/handler.js
+++ tutorial-step-1/handler.js
@@ -7,6 +7,11 @@ module.exports.hello = async (event) => {
     let body = JSON.parse(event.body)
 
     // 추가 도전과제: body가 { input: 숫자 } 가 맞는지 검증하고, 검증에 실패하면 응답코드 400 및 에러 메시지를 반환하는 코드를 넣어봅시다.
+    if(typeof body.input !== 'number')
+      return {
+        statusCode: 400,
+        body: "숫자가 아닙니다."
+      }
 
     inputValue = parseInt(body.input)
     outputValue = inputValue + 1
